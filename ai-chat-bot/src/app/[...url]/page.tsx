ebca3d43ef42cc6e324a5ab7bab4bb991988afd1
Saima Sanjeeda Khan <119809514+saima-khan1@users.noreply.github.com>
2024-09-26 10:40:30 +0200

    setup-application

---
+++ ai-chat-bot/src/app/[...url]/page.tsx
@@ -0,0 +1,39 @@
+import { ragChat } from "@/src/lib/rag-chat";
+import { redis } from "@/src/lib/redis";
+import React, { Component } from "react";
+import ChatWrapper from "@/src/components/ChatWrapper";
+
+interface PageProps {
+  params: {
+    url: string | string[] | undefined;
+  };
+}
+
+function reconstructUrl({ url }: { url: string[] }) {
+  const decodedComponents = url.map((component) =>
+    decodeURIComponent(component)
+  );
+  return decodedComponents.join("/");
+}
+
+const Page = async ({ params }: PageProps) => {
+  const reconstructedUrl = reconstructUrl({ url: params.url as string[] });
+
+  const isAlreadyIndexed = await redis.sismember(
+    "indexed-urls",
+    reconstructedUrl
+  );
+  const sessionId = "mock-session";
+
+  if (!isAlreadyIndexed) {
+    await ragChat.context.add({
+      type: "html",
+      source: reconstructedUrl,
+      config: { chunkOverlap: 50, chunkSize: 200 },
+    });
+    await redis.sadd("indexed-urls", reconstructedUrl);
+  }
+  return <ChatWrapper sessionId={sessionId} />;
+};
+
+export default Page;
